b8dd6d1afdc951d2e53d40e4f266f6f41731d14b
terry2262001 <terynguyen2262001@gmail.com>
2024-05-01 02:09:27 +0700

    update: framework

---
+++ ValidEmailFrameWork/ValidEmailFrameWork/ValidEmailFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  ValidEmailFrameWork.h
+//  ValidEmailFrameWork
+//
+//  Created by rta on 5/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValidEmailFrameWork.
+FOUNDATION_EXPORT double ValidEmailFrameWorkVersionNumber;
+
+//! Project version string for ValidEmailFrameWork.
+FOUNDATION_EXPORT const unsigned char ValidEmailFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidEmailFrameWork/PublicHeader.h>
+
+
